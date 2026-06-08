8c983fba722bc8f602a6cccdabf8026174ccf87a
Nico Dierking <nico-dierking@web.de>
2026-06-08 18:24:01 +0200

    app setup

---
+++ Evolvo/app/(auth)/signin.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const signin = () => {
+    return (
+        <View className='w-full h-full items-center justify-center'>
+            <Text>signin</Text>
+        </View>
+    )
+}
+
+export default signin
---
+++ Evolvo/app/(tabs)/lectures.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const lectures = () => {
+  return (
+    <View>
+      <Text>lectures</Text>
+    </View>
+  )
+}
+
+export default lectures
