3d5c00b6742856df4106958150da589cb6d8a092
Nico Dierking <nico-dierking@web.de>
2026-06-10 15:57:37 +0200

    implement error middleware

--- Evolvo-backend/app.js
+++ Evolvo-backend/app.js
@@ -5,12 +5,20 @@ import userRouter from "./routes/user.routes.js";
 import lectureRouter from "./routes/lectures.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectToDataBase from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
-//Arthurius0904
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser())
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/lectures", lectureRouter);
+
+app.use(errorMiddleware)
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Evolvo API!");
